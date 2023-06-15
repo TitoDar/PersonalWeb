6d9e4bed58e6dbdfebf4e77ddba0990cd25c7bc4
TitoDar <135117703+TitoDar@users.noreply.github.com>
2023-06-15 11:18:25 +0800

    Add files via upload

---
+++ core.js
@@ -0,0 +1,49 @@
+/**
+ * --------------------------------------------------------
+ * This demo was created using amCharts V4 preview release.
+ * 
+ * V4 is the latest installement in amCharts data viz
+ * library family, to be released in the first half of
+ * 2018.
+ *
+ * For more information and documentation visit:
+ * https://www.amcharts.com/docs/v4/
+ * --------------------------------------------------------
+ */
+
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.PieChart);
+
+// Add data
+chart.data = [{
+  "country": "Lithuania",
+  "litres": 501.9
+}, {
+  "country": "Czech Republic",
+  "litres": 301.9
+}, {
+  "country": "Ireland",
+  "litres": 201.1
+}, {
+  "country": "Germany",
+  "litres": 165.8
+}, {
+  "country": "Australia",
+  "litres": 139.9
+}, {
+  "country": "Austria",
+  "litres": 128.3
+}];
+
+// Add and configure Series
+var pieSeries = chart.series.push(new am4charts.PieSeries());
+pieSeries.dataFields.value = "litres";
+pieSeries.dataFields.category = "country";
+
+pieSeries.ticks.template.disabled = true;
+pieSeries.alignLabels = false;
+pieSeries.labels.template.text = "{value.percent.formatNumber('#.0')}%";
+pieSeries.labels.template.radius = am4core.percent(-40);
+pieSeries.labels.template.fill = am4core.color("white");
+
+chart.legend = new am4charts.Legend();
